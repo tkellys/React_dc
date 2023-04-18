f3be2592135c766db38699119129f9da5b1c7edc
jvitor691 <jvitormarques2@hotmail.com>
2023-04-17 21:47:09 -0300

    criando navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+//import './App.css'
 import {InputText} from "./componets/inputText";
 import {Titulo} from "./componets/h1title";
+import { MyNavbar } from './componets/MyNavbar';
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,7 +13,7 @@ function App() {
   return (
  //tag vazia se chama fragment
    <> 
-  
+  <MyNavbar/>
    </>
   )
 }
--- src/componets/MyNavbar.jsx
+++ src/componets/MyNavbar.jsx
@@ -1,10 +1,51 @@
 // instalar extensao do vscode que cria todo componente:Snippet ES7
 import React from 'react'
 
-const MyNavbar = () => {
+export const MyNavbar = () => {
+
+// criar um array com json onde vou colocar as infor q precisa ter na minha navBar
+    const menus = [
+        {
+            nome:'Produtos',
+            link:'/produtos'
+        },
+        {
+            nome:'Usuarios',
+            link:'/usuarios'
+        },{
+            nome:'Categorias'
+            link:'/categorias'
+        }
+    ]
+  const gerarMenus = () =>{
+    //pq usar o MAP e nao FOREACH? Pq quero gerar uma nova lista, o foreach altera a lista original
+    menus.map() //Gerar elementos de Li automatico.
+  }
+
   return (
-    <div>MyNavbar</div>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled">Disabled</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
   )
 }
-
-export default MyNavbar
